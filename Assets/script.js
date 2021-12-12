c4729c7e174902abe63f846f521bbdbf40514c67
steph <sjapodaca1103@gmail.com>
2021-12-11 19:07:30 -0800

    working on JS

--- Assets/script.js
+++ Assets/script.js
@@ -1,22 +1,150 @@
-class Quiz {
-    constructor(questions) {
-        this.score = 0;
-        this.questions = questions;
-        this.questionIndex = 0;
-    }
+const { endsWith } = require("sequelize/dist/lib/operators");
 
-    getQuestionIndex() {
-        return this.questions[this.questionIndex];
-    }
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("#choice-text"));
+const quizHeader = document.querySelector("quizHeader")
+const choice = document.querySelectorAll("btn")
+const questionCont = document.querySelector('#question')
 
-    guess(answer) {
-        if (this.getQuestionIndex().isCorrectAnswer(answer)) {
-            this.score++;
+var currentQuestion = 0;
+// var acceptingAnswers = true;
+var score = 0;
+var questionCounter = 0;
+var availableQuestions = [];
+
+var questions = [
+    {
+        question: 'What does HTML stand for?',
+        choice0: 'Hypertext Markup Language',
+        choice1: 'Hyper Makeup Language',
+        choice2: 'How To Make Language',
+        choice3: 'Hypertext Make Language',
+        answer: 0,
+    },
+    {
+        question: 'Cascading Style Sheets (CSS) does what to the page?',
+        choice0: 'Allows you to make the page interactive',
+        choice1: 'Conducts data analysis',
+        choice2: 'Adds style to a webpage',
+        choice3: 'Where the initial code goes to create website',
+        answer: 2,
+    },
+    {
+        question: 'Where do you place the external CSS link in HTML?',
+        choice0: 'The very end of the body',
+        choice1: 'In the <h1>',
+        choice2: 'In the <footer>',
+        choice3: 'In the <head>',
+        answer: 3,
+    },
+];
+
+
+var startQuiz = function (event) {
+    event.preventDefault()
+    setTime()
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = ["What does HTML stand for?", "Cascading Style Sheets (CSS) does what to the page?",
+        "Where do you place the external CSS link in HTML?"];
+    getNewQuestion();
+};
+
+let time = 10;
+let quizTimeInMinutes = time * 60 * 60;
+let quizTime = quizTimeInMinutes / 60;
+let counting = document.getElementById("timer");
+
+function setTime() {
+    var quizTimer = setInterval(function () {
+        if (quizTime <= 0) {
+            clearInterval(quizTimer);
+            secondsLeft = 0
+            questionCont.style.display = "none"
+            end.style.display = "block"
+
+            // showScores();
+        } else {
+            quizTime--;
+            let sec = Math.floor(quizTime % 60);
+            let min = Math.floor(quizTime / 60) % 60;
+            counting.innerHTML = `TIME: ${min} : ${sec}`;
         }
-        this.questionIndex++;
+    }, 1000);
+
+    var select = function (event) {
+        var btn = event.target.textContent
+        if (btn === questions[currentQuestion].answer) {
+            alert("correct")
+            score++
+
+        } else {
+            alert("Wrong")
+            secondsLeft -= 30
+        }
+        currentQuestion++
+        if (currentQuestion === listQuestion.length) {
+            clearInterval(quizTimer)
+            questionCont.style.display = "none"
+            end.style.display = "none"
+            leaderbox.style.display = "none"
+        } else {
+            getNewQuestion()
+        }
     }
 
-    isEnded() {
-        return this.questionIndex === this.questions.length;
-    }
+    // var getNewQuestion = function () {
+    //     if (availableQuestions.length === 0 || questionCounter >= Max_Questions) {
+    //         return window.location.assign();
+
+    //     };
+
+    //     questionCounter++;
+    //     var questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    //     currentQuestion = availableQuestions[questionIndex];
+    //     question.innerText = currentQuestion.question;
+
+    //     choices.forEach(choices, function (choices) {
+    //         var number = choices.dataset['number'];
+    //         choices.innerText = currentQuestion['choice' + number];
+    //     });
+
+
+    //     availableQuestions.splice(questionIndex, 1);
+    //     acceptingAnswers = true;
+    // };
+
+    // choices.forEach(choices, function () {
+    //     choices.addEventListener('click', function (event) {
+    //         if (!acceptingAnswers) return;
+
+    //         acceptingAnswers = false;
+    //         var selectedChoice = event.target;
+    //         selectedAnswer = selectedChoice.dataset['number'];
+    //         getNewQuestion();
+    //     });
+
+
+    // });
+
+    // let time = 10;
+    // let quizTimeInMinutes = time * 60 * 60;
+    // let quizTime = quizTimeInMinutes / 60;
+    // let counting = document.getElementById("timer");
+
+    // function startCountdown() {
+    //     let quizTimer = setInterval(function () {
+    //         if (quizTime <= 0) {
+    //             clearInterval(quizTimer);
+    //             showScores();
+    //         } else {
+    //             quizTime--;
+    //             let sec = Math.floor(quizTime % 60);
+    //             let min = Math.floor(quizTime / 60) % 60;
+    //             counting.innerHTML = `time: ${min} : ${sec}`;
+    //         }
+    //     }, 1000);
 }
+setTime();
+startQuiz();
+
